fcae5b9f6175c4eeb2e84079e165451b22cece59
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-08-17 15:04:47 +0800

    footer color

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="text-white py-3 flex justify-center items-center" style="background-color: #09CA16;">
+<footer class="text-white py-3 flex justify-center items-center" style="background-color: #09ca59ff;">
     <p class="text-sm">&copy; 2025 PCAppTrack All Rights Reserved</p>
 </footer>
