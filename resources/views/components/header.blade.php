bdd466fdc092a25b1a70f1fd1a1eb221c9b24244
jaor13 <jaor2022-6131-98317@bicol-u.edu.ph>
2025-10-11 01:28:05 +0800

    modal next:

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,7 +4,7 @@
     <div class="w-full max-w-7xl mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8 py-2 sm:py-2.5">
         <!-- Bagong Pilipinas Logo -->
         <div class="flex items-center flex-shrink-0">
-            <img src="{{ asset('images/img/bagong-pilipinas.jpg') }}" alt="Bagong Pilipinas Logo"
+            <img src="{{ asset('images/img/bagong-pilipinas.png') }}" alt="Bagong Pilipinas Logo"
                 class="h-10 sm:h-12 md:h-14 lg:h-16 w-auto object-contain" style="margin-left:-10px;"
                 onerror="this.onerror=null; this.src='{{ asset('images/cfidpgp.png') }}';">
         </div>
