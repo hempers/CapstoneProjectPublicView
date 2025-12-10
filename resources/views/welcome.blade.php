147107d31ee13371627d499145feebb9aaf9ffda
jaor13 <jaor2022-6131-98317@bicol-u.edu.ph>
2025-12-10 14:52:01 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -755,8 +755,8 @@
 
                             timelineItem.innerHTML = `
                                                 <div class="relative">
+                                                    ${isLastItem ? '' : '<div class="absolute left-0 top-[18px] bottom-0 w-0.5 ' + lineColor + '"></div>'}
                                                     <div class="absolute -left-[6px] top-[6px] h-3 w-3 rounded-full ${dotColor} border-2 ${dotBorder}"></div>
-                                                    ${!isEndOfTimeline ? '<div class="absolute left-0 top-[18px] bottom-0 w-0.5 ' + lineColor + '"></div>' : ''}
                                                     <div class="ml-6 sm:ml-8">
                                                         <!-- Status badge at top -->
                                                         <div class="${stageClass} inline-block text-xs font-medium px-3 py-1 rounded-xl mb-2">
